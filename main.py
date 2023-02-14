5d0365971c01b53f4952df9260c314fc9f14b412
Damoon Robatian <damoon.robatian@gmail.com>
2023-02-13 21:29:25 -0500

    Minor update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn
+import seaborn as sns
 import warnings
 warnings.filterwarnings("ignore")
 import os
@@ -29,7 +29,7 @@ both = pd.concat([train, test], axis=0).reset_index(drop=True)
 #%%
 xtick_loc = np.arange(len(both["Activity"].unique()))
 bar_width = .2
-# fig, ax = plt.subplot()
+
 plt.bar(x=xtick_loc - .2, height=both["Activity"].value_counts(), width = bar_width, label='Both')
 plt.bar(x=xtick_loc , height=train["Activity"].value_counts(),  width = bar_width, label='Train')
 plt.bar(x=xtick_loc + .2, height=test["Activity"].value_counts(),  width = bar_width, label='Test')
@@ -43,16 +43,47 @@ plt.xticks(ticks = xtick_loc, labels = both["Activity"].value_counts().index, ro
 plt.legend()
 #%%    
 # 2. Values of 'subject' in the training and test sets 
-train['subject'].sort_values().unique() 
-test['subject'].sort_values().unique() 
-
-
-uniq_subjects = both["subject"].sort_values().unique()
-
+train_subjects = train['subject'].sort_values().unique() 
+test_subjects = test['subject'].sort_values().unique() 
 #%%
 rows_per_subject = []
-for d in both.groupby('subject'):
-    rows_per_subject.append([d[0], d[1].shape[0]])    
+for subject, sub_df in both.groupby('subject'):
+    if subject in train_subjects:
+        rows_per_subject.append([str(subject), sub_df.shape[0], "Train"])  # str(subject) to be able to sort the plot later
+    else:
+        rows_per_subject.append([str(subject), sub_df.shape[0], "Test"])
+    
+rows_per_subject = pd.DataFrame(rows_per_subject, columns = ['subject', 'num_of_rows', 'label'])
+rows_per_subject_sorted = rows_per_subject.sort_values('num_of_rows')
+rows_per_subject_sorted
+#%% For some reason values are not sorted in the plot!!!
+colors1 = ['red' if i == 'Test' else 'blue' for i in rows_per_subject_sorted['label']] 
+labels = ['Train', 'Test']
+handles = [plt.Rectangle((0,0), 1, 1, color='blue'),
+           plt.Rectangle((0,0), 1, 1, color='red')]
 
-rows_per_subject = pd.DataFrame(rows_per_subject, columns = ['subject', 'num_of_rows']).set_index('subject')
-rows_per_subject
+plt.bar(x='subject', height = 'num_of_rows', data = rows_per_subject_sorted, color = colors1)
+plt.title("Total Number of the Rows for Every Subject")
+plt.xticks(ticks = np.arange(30), labels = rows_per_subject_sorted['subject'], size = 7)
+plt.legend(handles, labels)
+
+#%%
+rows_per_subject.min()
+rows_per_subject.max()
+rows_per_subject.mean()
+rows_per_subject.sort_values('num_of_rows')
+#%%
+
+
+
+
+
+
+
+
+
+
+
+
+
+
